f53edac846a4bd04047037b7ffc586c9a4b8ea6d
PAPATIRI <syariftaufik11@gmail.com>
2025-03-04 21:50:52 +0700

    refactor(table): seperate definition of columns for data table and add dark class on html just for eyes safety

--- src/pages/projects/index.vue
+++ src/pages/projects/index.vue
@@ -1,8 +1,7 @@
 <script setup lang="ts">
-import type { ColumnDef } from '@tanstack/vue-table'
-import { RouterLink } from 'vue-router'
 import { usePageStore } from '@/stores/page'
 import { projectQuery, type Projects } from '@/utils/supabaseQuery'
+import { columns } from '@/utils/tableColumns/projectsColumns'
 
 usePageStore().pageData.title = 'Projects Page'
 
@@ -15,38 +14,6 @@ const getProjects = async () => {
   projects.value = data
 }
 await getProjects()
-
-const columns: ColumnDef<Projects[0]>[] = [
-  {
-    accessorKey: 'name',
-    header: () => h('div', { class: 'text-left' }, 'Name'),
-    cell: ({ row }) => {
-      return h(
-        RouterLink,
-        { to: `/projects/${row.original.slug}`, class: 'text-left font-medium w-full block' },
-        () => row.getValue('name'),
-      )
-    },
-  },
-  {
-    accessorKey: 'status',
-    header: () => h('div', { class: 'text-left' }, 'Status'),
-    cell: ({ row }) => {
-      return h('div', { class: 'text-left font-medium' }, row.getValue('status'))
-    },
-  },
-  {
-    accessorKey: 'collaborators',
-    header: () => h('div', { class: 'text-left' }, 'Collaborators'),
-    cell: ({ row }) => {
-      return h(
-        'div',
-        { class: 'text-left font-medium' },
-        JSON.stringify(row.getValue('collaborators')),
-      )
-    },
-  },
-]
 </script>
 
 <template>
