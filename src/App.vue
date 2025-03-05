81bbc58c9c1524bdb4f3ae39d1ad6d1fd2019a95
PAPATIRI <syariftaufik11@gmail.com>
2025-03-05 21:33:10 +0700

    feat(errorhandling): using vue's hook onErrorCaptured to handle uncaught javascripts error in vue js

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <script setup lang="ts">
-import { storeToRefs } from 'pinia'
 import { useErrorStore } from './stores/error'
 
-const { activeError } = storeToRefs(useErrorStore())
+const errorStore = useErrorStore()
+
+onErrorCaptured((error) => {
+  errorStore.setError({ error })
+})
 </script>
 
 <template>
   <AuthLayout>
-    <AppErrorPage v-if="activeError" />
+    <AppErrorPage v-if="errorStore.activeError" />
     <RouterView v-slot="{ Component, route }">
       <Suspense v-if="Component" timeout="0">
         <Component :is="Component" :key="route.name"></Component>
